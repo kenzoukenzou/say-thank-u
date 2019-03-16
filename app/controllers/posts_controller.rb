1504e28d78a61b0506371f482e0b3df23bba0e1e
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-03-16 10:02:22 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+
+  before_action :auth_user
+  before_action :correct_user, {only: [:edit, :update, :destroy]}
+  def index
+  	@posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+  	@post = Post.find_by(id:params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  	@post = Post.new(content:params[:content],user_id: @current_user.id)
+  	@post.save
+  	redirect_to('/posts/index')
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    flash[:notice] = "投稿を編集しました！"
+    redirect_to("/posts/index")
+  end
+
+  def destroy 
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました！"
+    redirect_to("/posts/index")
+  end
+
+  def correct_user
+    @post= Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません！"
+      redirect_to("/posts/index")
+    end
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,93 @@
+class UsersController < ApplicationController
+
+  before_action :auth_user, {only: [:index, :show]}
+  before_action :login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :correct_user, {only: [:edit, :update]}
+
+  def index
+  end
+
+  def new
+  	@user = User.new
+  end
+
+
+  def create
+  	@user = User.new(name: params[:name], 
+      email: params[:email],
+      password: params[:password])
+    
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}",image.read)
+    else 
+       @user.image_name = "cat.jpg"
+    end
+
+  	if @user.save
+      session[:user_id] = @user.id
+  		flash[:notice] = "ユーザー登録が完了しました"
+  		redirect_to('/posts/index')
+  	else
+  		render("/users/new")
+  	end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}",image.read)
+    end
+
+    @user.save
+    flash[:notice] = "ユーザー編集が完了しました！"
+    redirect_to("/users/#{@user.id}")
+  end
+
+
+  def show
+    @user = User.find_by(id:params[:id])
+  end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "#{@user.name}さんこんにちは！"
+      redirect_to("/posts/index")
+    else
+      @error = "ユーザーが見つかりませんでした！"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました！"
+    redirect_to("/login")
+  end
+
+  def correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません！"
+      redirect_to("/posts/index")
+    end
+  end
+
+end
