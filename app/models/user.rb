1774e254b3f892328e326663e44da2136fdfe937
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-03-20 13:35:22 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 	validates :email, {presence: true, uniqueness: true}
 	validates :password, {presence: true}
 
+	has_one_attached :image
+
 	def posts
 		return Post.where(user_id: self.id)
 	end
