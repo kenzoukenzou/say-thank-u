bffd1b24cb0b3e80095d0b34921e0d860a9c6e23
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-04-09 06:08:27 +0900

    モダールウィンドウ実装

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -3,4 +3,11 @@ $(function(){
 				$("header").toggleClass('open');
 		$("nav").fadeToggle(500);
 	});
+	// modal window
+	$('#openModal').click(function(){
+		$('#modalArea').fadeIn();
+		});
+	$('#closeModal , #modalBg').click(function(){
+		$('#modalArea').fadeOut();
+	});
  });
