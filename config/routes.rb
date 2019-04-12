1d7cbd40410ad48a2f04291379b016824bce924f
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-04-12 16:27:55 +0900

    herokuエラー対処

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get 'posts/:id/edit' => 'posts#edit'
   post 'posts/:id/update' => 'posts#update'
   post "posts/:id/destroy" => "posts#destroy"
+  
   get '/' => 'home#top'
   resources :home
   root 'home#top'
