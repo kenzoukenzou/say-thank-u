2ca75f0bbccce046571ec89d13aaa07706129aa9
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-04-17 20:18:56 +0900

    active storage保存先を一時的にlocalに変更

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -27,4 +27,23 @@ describe '投稿管理機能', type: :system do
 			end
 		end
 	end
+
+	describe 'ユーザー詳細表示機能' do
+		before do
+			FactoryBot.create(:post, content: 'first post', user_id: user_a.id)
+			visit login_path
+			fill_in 'メールアドレス', with: login_user.email
+			fill_in 'パスワード', with: login_user.password
+			click_button 'ログイン'
+			visit user_path(login_user)
+		end
+
+		context 'ユーザーAがログインしている時' do
+			let(:login_user) { user_a }
+			
+			it 'ユーザーAが作成した投稿が表示される' do
+				expect(page).to have_content 'first post'
+			end
+		end
+	end
 end
