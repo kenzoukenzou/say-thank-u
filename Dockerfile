89cebe924e771af95dcf3c11dbecbc4f468e6b11
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-04-27 13:27:57 +0900

    Docker環境構築

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ruby:2.5
+RUN apt-get update && apt-get install -y nodejs --no-install-recommends && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y postgresql-client --no-install-recommends && rm -rf /var/lib/apt/lists/*
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+
+WORKDIR /saythanks
+
+ADD Gemfile /saythanks/Gemfile
+ADD Gemfile.lock /saythanks/Gemfile.lock
+
+RUN gem install bundler
+RUN bundle install
+
+ADD . /saythanks
+
+COPY entrypoint.sh /usr/bin/
+RUN chmod +x /usr/bin/entrypoint.sh
+ENTRYPOINT ["entrypoint.sh"]
+EXPOSE 3000
+CMD ["rails", "server", "-b", "0.0.0.0"]
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_04_20_084113) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -51,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_04_20_084113) do
     t.string "uid"
     t.string "token"
     t.string "meta"
-    t.datetime "ast_sign_in_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
@@ -63,4 +65,5 @@ ActiveRecord::Schema.define(version: 2019_04_20_084113) do
     t.datetime "remember_created_at"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
